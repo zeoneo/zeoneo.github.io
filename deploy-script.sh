22fb191eeda467f5f0b1dfc251dfb065a39a1c99
zeo <zeo29021992@gmail.com>
2020-03-07 23:20:38 +0530

    Change gitbhub action

--- deploy-script.sh
+++ deploy-script.sh
@@ -17,6 +17,7 @@ git config user.email "${GITHUB_ACTOR}@bots.github.com"
 git checkout "$target_branch"
 git rebase "${remote_name}/${main_branch}"
 
+npm install -g gatsby
 npm run build
 git add "$build_dir"
 
