0dc8b8d45f0a5b801c5d6e019988a7fe116ed58c
zeo <zeo29021992@gmail.com>
2020-03-07 23:08:55 +0530

    Add auto deploy

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,8 +2,8 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
-export default ({ data }) => (
-  <Layout>
+export default ({ data, location }) => (
+  <Layout location={location}>
     <h1>About {data.site.siteMetadata.title}</h1>
     <p>
       We're the only site running on your computer dedicated to showing the best
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ class BlogIndex extends React.Component {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
     const posts = data.allMarkdownRemark.edges
-
+    console.log(JSON.stringify(this.props))
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO
