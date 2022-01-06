dd50645ab1f908f0bd21c348c2f9f327c8608694
zeo <zeo29021992@gmail.com>
2022-01-06 09:12:31 +0530

    Fix

--- deploy-script.sh
+++ deploy-script.sh
@@ -19,7 +19,7 @@ git checkout "$main_branch"
 npm install
 npm run build
 git add "$build_dir"
-git add package-lock.json
+git checkout HEAD -- package-lock.json
 
 git commit -m "updated GitHub Pages"
 if [ $? -ne 0 ]; then
