397a02db03f940453a3195246f7c5e8380ee4da3
zeoneo <zeoneo@bots.github.com>
2024-01-27 13:00:31 +0000

    updated GitHub Pages

---
+++ component---src-pages-about-js-c2fe73009ac29d7375dd.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[682],{5465:function(t,e,a){a.r(e);var o=a(7294),n=a(8678);e.default=t=>{let{data:e,location:a}=t;return o.createElement(n.Z,{location:a},o.createElement("h1",null,"About ",e.site.siteMetadata.title),o.createElement("p",null,"We're the only site running on your computer dedicated to showing the best photos and videos of pandas eating lots of food."))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-c2fe73009ac29d7375dd.js.map
---
+++ component---src-pages-contact-js-0fac4a020a94be505f05.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[501],{9220:function(e,t,a){a.r(t),a.d(t,{default:function(){return s}});var r=a(7294),l=a(1082),n=e=>{let{headerText:t}=e;return r.createElement("h1",null,t)},s=()=>r.createElement("div",{style:{color:"teal"}},r.createElement(l.Link,{to:"/"},"Home"),r.createElement(n,{headerText:"Contact"}),r.createElement("p",null,"Send us a message!"))}}]);
+//# sourceMappingURL=component---src-pages-contact-js-0fac4a020a94be505f05.js.map
